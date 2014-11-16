a99729cbaaecb34748aa274d3690a182f5e07c85
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-16 20:00:00 +0200

    Remoce CSS because we are not using any.

--- includes/scripts.php
+++ includes/scripts.php
@@ -44,32 +44,6 @@ function edd_members_admin_scripts( $hook ) {
 		$ui_style = ( 'classic' == get_user_option( 'admin_color' ) ) ? 'classic' : 'fresh';
 		wp_enqueue_style( 'jquery-ui-css', trailingslashit( EDD_PLUGIN_URL ) . 'assets/css/jquery-ui-' . $ui_style . '.css' );
 	}
-
-    /**
-     * @todo		This block loads styles or scripts explicitly on the
-     *				EDD settings page.
-     */
-	if( $hook == $edd_settings_page ) {
-		wp_enqueue_script( 'edd-members-admin-js', EDD_MEMBERS_URL . '/assets/js/admin' . $suffix . '.js', array( 'jquery' ) );
-		wp_enqueue_style( 'edd-members-admin-css', EDD_MEMBERS_URL . '/assets/css/admin' . $suffix . '.css' );
-	}
 	
 }
 add_action( 'admin_enqueue_scripts', 'edd_members_admin_scripts', 100 );
-
-
-/**
- * Load frontend scripts
- *
- * @since       1.0.0
- * @return      void
- */
-function edd_members_scripts( $hook ) {
-
-	// Use minified libraries if SCRIPT_DEBUG is turned off
-	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-
-	wp_enqueue_script( 'edd-members-js', EDD_MEMBERS_URL . '/assets/js/scripts' . $suffix . '.js', array( 'jquery' ) );
-	wp_enqueue_style( 'edd-members-css', EDD_MEMBERS_URL . '/assets/css/styles' . $suffix . '.css' );
-}
-add_action( 'wp_enqueue_scripts', 'edd_members_scripts' );
