cb7d5dd7090f7914371be4827856fcbf249e8368
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-16 19:57:39 +0200

    Add datepicker.

--- includes/scripts.php
+++ includes/scripts.php
@@ -27,6 +27,23 @@ function edd_members_admin_scripts( $hook ) {
 
 	// Use minified libraries if SCRIPT_DEBUG is turned off
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	
+	// Enqueue date picker in user profile page
+	if( 'user-edit.php' == $hook ) {
+	
+		// Add datepicker
+		wp_enqueue_script( 'edd-members-datepicker-settings', EDD_MEMBERS_URL . '/assets/js/admin' . $suffix . '.js', array( 'jquery-ui-datepicker' ) );
+		
+		// Localize dateformat
+		wp_localize_script( 'edd-members-datepicker-settings', 'datepicker_settings_vars', array(
+			'dateformat' => apply_filters( 'edd_members_datepicker_date', 'mm/dd/yy' )
+			)
+		);
+		
+		// Add styles from EDD plugin
+		$ui_style = ( 'classic' == get_user_option( 'admin_color' ) ) ? 'classic' : 'fresh';
+		wp_enqueue_style( 'jquery-ui-css', trailingslashit( EDD_PLUGIN_URL ) . 'assets/css/jquery-ui-' . $ui_style . '.css' );
+	}
 
     /**
      * @todo		This block loads styles or scripts explicitly on the
@@ -36,6 +53,7 @@ function edd_members_admin_scripts( $hook ) {
 		wp_enqueue_script( 'edd-members-admin-js', EDD_MEMBERS_URL . '/assets/js/admin' . $suffix . '.js', array( 'jquery' ) );
 		wp_enqueue_style( 'edd-members-admin-css', EDD_MEMBERS_URL . '/assets/css/admin' . $suffix . '.css' );
 	}
+	
 }
 add_action( 'admin_enqueue_scripts', 'edd_members_admin_scripts', 100 );
 
