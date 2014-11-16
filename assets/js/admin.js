06e757399a7ea2d65b16303bee92eabf9378430b
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-16 19:51:08 +0200

    Add admin js to handle datepicker.

---
+++ assets/js/admin.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+	
+	// date picker
+	//var dateFormat = 'mm/dd/yy';
+	$('.edd_members_datepicker').datepicker({
+		dateFormat: datepicker_settings_vars.dateformat
+	});
+});
