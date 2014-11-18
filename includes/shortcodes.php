a2f7d3c77a54d6d6f6df62ffa50465cb4a9d746a
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-18 22:42:24 +0200

    Use ready to go function.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -21,24 +21,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function edd_members_expire_date_shortcode() {
 	
-	if( is_user_logged_in() ) {
-	
-		// Get current user id
-		$user_id = get_current_user_id();
-		
-		// Get expire date
-		$expire_date = get_the_author_meta( '_edd_members_expiration_date', $user_id );
-		
-		// Return expire_date if there is one
-		if ( !empty( $expire_date ) ) {
-			$edd_members_expire_date = date_i18n( get_option( 'date_format' ), $expire_date );
-		} else {
-			$edd_members_expire_date = __( 'Unknown', 'edd-members' );
-		}
-		
-		return $edd_members_expire_date;
-		
+	if ( false != edd_members_get_expire_date() ) {
+		return edd_members_get_expire_date();
 	}
+	
+	return '';
 
 }
 add_shortcode( 'edd_members_expire_date', 'edd_members_expire_date_shortcode' );
