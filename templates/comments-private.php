33d66bae96de61dc56c4b1e3af60e973efac5b16
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-11-30 15:03:02 +0200

    Add empty comments template.

---
+++ templates/comments-private.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template for private comments. Leave empty for now.
+ *
+ * This can be overwritten in themes 'edd-members-templates' folder.
+ *
+ * @package     EDDMembers\Templates
+ * @since       1.0.0
+ */
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+do_action( 'edd_members_comments_private' ); // Add action hook
